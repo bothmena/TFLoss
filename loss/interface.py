556999af202777e61601324f87d0f92ff52c33f4
Aymen Ben Othmen <aymenbenothmenabo@gmail.com>
2019-09-25 20:02:07 +0200

    feat: add reduction argument to calculate fct

--- loss/interface.py
+++ loss/interface.py
@@ -3,8 +3,8 @@ from abc import ABCMeta, abstractmethod
 
 class Loss(metaclass=ABCMeta):
     @abstractmethod
-    def calculate(self, y_batch, y_hat_batch):
+    def calculate(self, y_batch, y_hat_batch, reduction: str = None):
         raise NotImplementedError()
 
-    def __call__(self, y_batch, y_hat_batch, *args, **kwargs):
-        return self.calculate(y_batch, y_hat_batch)
+    def __call__(self, y_batch, y_hat_batch, reduction: str = None, *args, **kwargs):
+        return self.calculate(y_batch, y_hat_batch, reduction)
